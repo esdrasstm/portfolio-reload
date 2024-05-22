b4dec0eb1094dc1319f7d9d22d6af38c7f39e558
Esdras Tomé <esdras.dsgn@gmail.com>
2024-05-22 12:15:10 -0300

    responsividade

---
+++ script.js
@@ -0,0 +1,6 @@
+const hamburger = document.querySelector(".hamburger");
+const nav = document.querySelector(".lista");
+
+hamburger.addEventListener("click", () => nav.classList.toggle("active"));
+
+
